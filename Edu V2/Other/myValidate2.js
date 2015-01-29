2194a6b47973d3da1c8a70a03037f730a03aedc0
kabue <mckabue@gmail.com>
2015-01-30 00:51:31 +0300

    k

---
+++ Edu V2/Other/myValidate2.js
@@ -0,0 +1,125 @@
+﻿$(document).ready(function () {
+    $('#loginForm, #loginForm2, #signupForm')
+        .bootstrapValidator({
+            //live: 'disabled',
+            submitButtons: '[type="submit"]',
+            message: 'Invalid input',
+            fields: {
+                username: {
+                    message: 'The username is not valid',
+                    validators: {
+                        notEmpty: {
+                            message: 'The username is required and can\'t be empty'
+                        },
+                        stringLength: {
+                            max: 30,
+                            message: 'The username must be more than 6 and less than 30 characters long'
+                        },
+                        /*remote: {
+                            url: 'remote.php',
+                            message: 'The username is not available'
+                        },*/
+                        regexp: {
+                            regexp: /^[a-zA-Z0-9_\.]+$/,
+                            message: 'The username can only consist of alphabetical, number, dot and underscore'
+                        }
+                    }
+                },
+                firstname: {
+                    message: 'The first name is not valid',
+                    validators: {
+                        notEmpty: {
+                            message: 'The first name is required and can\'t be empty'
+                        },
+                        stringLength: {
+                            max: 30,
+                            message: 'The first name can\'t be more than 30 characters long'
+                        },
+                        /*remote: {
+                            url: 'remote.php',
+                            message: 'The username is not available'
+                        },*/
+                        regexp: {
+                            regexp: /^[a-zA-Z0-9_\.]+$/,
+                            message: 'The username can only consist of alphabetical, number, dot and underscore'
+                        }
+                    }
+                },
+                lastname: {
+                    message: 'The last name is not valid',
+                    validators: {
+                        notEmpty: {
+                            message: 'The last name is required and can\'t be empty'
+                        },
+                        stringLength: {
+                            max: 30,
+                            message: 'The last name can\'t be more than 30 characters long'
+                        },
+                        /*remote: {
+                            url: 'remote.php',
+                            message: 'The username is not available'
+                        },*/
+                        regexp: {
+                            regexp: /^[a-zA-Z0-9_\.]+$/,
+                            message: 'The username can only consist of alphabetical, number, dot and underscore'
+                        }
+                    }
+                },
+                email: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The email address is required and can\'t be empty'
+                        },
+                        emailAddress: {
+                            message: 'The input is not a valid email address'
+                        }
+                    }
+                },
+                password: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The password is required and can\'t be empty'
+                        },
+                        stringLength: {
+                            min: 6,
+                            max: 30,
+                            message: 'The password must be more than 6 and less than 30 characters long'
+                        }
+                    }
+                },
+                confirmpassword: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Please confirm your password'
+                        },
+                        identical: {
+                            field: 'password',
+                            message: 'The password misMatch'
+                        }
+                    }
+                },
+				falcuty: {
+                validators: {
+                    notEmpty: {
+                        message: 'The country is required and can\'t be empty'
+                    }
+                }
+            }
+            }
+        })
+        .on('success.form.bv', function (e) {
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function (result) {
+                console.log(result);
+            }, 'json');
+        });
+});
